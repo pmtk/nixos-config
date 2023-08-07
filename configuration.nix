5f614a9f18cbe82199d421e0058500d6f983169d
Patryk Matuszak <pmtk@redhat.com>
2023-08-07 11:18:06 +0200

    enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -64,6 +64,8 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  hardware.bluetooth.enable = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
